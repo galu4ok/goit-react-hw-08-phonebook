6583add9e8e97509b0e41b5bf32263f86f9b125b
Galyna Shevchenko <galu4ok@gmail.com>
2023-09-30 16:29:58 +0300

    selectors optimization and adding contacts count optionally

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,10 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { changeFilter } from 'redux/filterSlice';
-import { getFilter } from 'redux/selectors';
+import { selectFilter } from 'redux/selectors';
 import { FilterInput } from './Filter.styled';
 
 export const Filter = () => {
-  const filter = useSelector(getFilter);
+  const filter = useSelector(selectFilter);
   const dispatch = useDispatch();
   const onChange = evt => {
     dispatch(changeFilter(evt.target.value));
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,19 @@
-export const getContacts = state => state.contacts;
-export const getFilter = state => state.filter;
+import { createSelector } from '@reduxjs/toolkit';
+export const selectContacts = state => state.contacts;
+export const selectFilter = state => state.filter;
+
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
+
+export const selectContactsCount = createSelector(
+  [selectContacts],
+  contacts => {
+    return contacts.length;
+  }
+);

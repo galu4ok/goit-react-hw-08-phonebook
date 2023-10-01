e41445cc0f5a39b499c1d1e500969b80f989e881
Galyna Shevchenko <galu4ok@gmail.com>
2023-10-01 03:41:26 +0300

    add operations for adding & removal contacts and perform HTTP requests

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -3,19 +3,42 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://65186ac7818c4e98ac5fc3b9.mockapi.io';
 
-//виконуємо HTTP-запит та отримуємо усі контакти
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      // HTTP-запит
+      // виконуємо HTTP-запит та отримуємо усі контакти
       const response = await axios.get('/contacts');
       // При успішному запиті повертаємо проміс із даними
+      console.log(response.data);
       return response.data;
-      console.log('response.data', response.data);
     } catch (e) {
       // При помилці запиту повертаємо проміс,який буде відхилений з текстом помилки
       return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', contact);
+      console.log(response.data);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, { rejectWithValue }) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (e) {
+      return rejectWithValue(e.message);
     }
   }
 );

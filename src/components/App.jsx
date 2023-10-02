b78673fb195a3b37e725db04bd5e3d4a6a4020fd
Galyna Shevchenko <galu4ok@gmail.com>
2023-10-02 06:47:12 +0300

    create PrivateRoute and RestrictedRoute components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
-import { lazy } from 'react';
+import { useAuth } from 'hooks/useAuth';
+import { lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+import { refreshUser } from 'redux/auth/operations';
 import Layout from './Layout/Layout';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
 const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
@@ -8,14 +13,40 @@ const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
 const ContactsPage = lazy(() => import('../pages/ContactsPage/ContactsPage'));
 
 export const App = () => {
-  return (
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser);
+  }, [dispatch]);
+  return isRefreshing ? (
+    <b>Fetching user data...</b>
+  ) : (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/contacts" element={<ContactsPage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={RegisterPage}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute redirectTo="/contacts" component={LoginPage} />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={ContactsPage} />
+            }
+          />
         </Route>
       </Routes>
     </>
